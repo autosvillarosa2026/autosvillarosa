9b3b61aebe72539b8605c893310713a9a311fe4a
autosvillarosa <autosvillarosa.app@gmail.com>
2026-02-19 19:54:00 +0100

    PRUEBA 1

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Autos Villa Rosa 2030',
+      title: 'Autos Villa Rosa 20000',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Autos Villa Rosa 2030'),
+      home: const MyHomePage(title: 'Autos Villa Rosa 20000'),
     );
   }
 }
