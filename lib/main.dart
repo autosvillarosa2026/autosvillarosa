18e08a24751e93fc2db297f412ccb39237cb1e2f
autosvillarosa <autosvillarosa.app@gmail.com>
2026-02-23 19:59:56 +0100

    PRUEBA 100

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CULO',
+      title: 'Prueba 100',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'CULO'),
+      home: const MyHomePage(title: 'Prueba 100'),
     );
   }
 }
